58e41c2a400a8869a77ab9ddbf4824047fe64249
Huy Phan <huy.phan2210@outlook.com>
2024-05-04 09:23:31 +0700

    Generate swagger.json

--- JobsListing.Server/Program.cs
+++ JobsListing.Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "jobs-listing-api", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -13,7 +20,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "jobs-listing-api v1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +31,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Save the Swagger JSON to a file
+var filePath = Path.Combine(app.Environment.ContentRootPath, "swagger.json");
+var swaggerProvider = app.Services.GetRequiredService<ISwaggerProvider>();
+using (var writer = new StreamWriter(filePath))
+{
+    var swaggerDoc = swaggerProvider.GetSwagger("v1");
+    swaggerDoc.SerializeAsJson(writer.BaseStream, new Microsoft.OpenApi.OpenApiSpecVersion());
+}
 
 app.Run();
